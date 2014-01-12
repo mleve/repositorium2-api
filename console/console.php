a6b63a213d66d4cf7028f6f8debc9170c5d01f03
LalSoong <nataliandrea@ug.uchile.cl>
2014-01-11 21:17:46 -0300

    Moira's House 21:17 xD

---
+++ console/console.php
@@ -0,0 +1,26 @@
+<?php
+  
+?>
+
+<div class="tabbable tabs-left">
+	<ul class="nav nav-tabs">
+		<li class="active"><a href="#lA" data-toggle="tab">My
+				applications</a>
+		</li>
+		<li><a href="#lB" data-toggle="tab">My criteria</a>
+		</li>
+		<li><a href="#lC" data-toggle="tab">My documents</a>
+		</li>
+	</ul>
+	<div class="tab-content">
+		<div class="tab-pane active" id="lA">
+			<p>I'm in Section A.</p>
+		</div>
+		<div class="tab-pane" id="lB">
+			<p>Howdy, I'm in Section B.</p>
+		</div>
+		<div class="tab-pane" id="lC">
+			<p>What up Moira, this is Section C.</p>
+		</div>
+	</div>
+</div>
