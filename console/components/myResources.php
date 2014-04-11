0c9cb9677f3555e2194f60138ee13b0ee2a32798
Mario <mleve@ug.uchile.cl>
2014-04-11 07:37:59 -0300

    primera version de la consola implementada

---
+++ console/components/myResources.php
@@ -0,0 +1,20 @@
+<ul class="nav nav-pills" >
+  <li class="active">
+    <a href="#">
+      <span class="badge pull-right">42</span>
+      My documents
+    </a>
+  </li>
+  <li>
+    <a href="#">
+      <span class="badge pull-right">16</span>
+      My criteria
+    </a>
+  </li>
+  <li>
+    <a href="#">
+      <span class="badge pull-right">16</span>
+      My Apps
+    </a>
+  </li>
+</ul>
